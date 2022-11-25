570cc681095fdaba3b9f42c8987f62b83519a48a
eftinur <eftinur90@gmail.comm>
2022-11-25 09:02:54 +0600

    added mongoDB config

--- index.js
+++ index.js
@@ -9,6 +9,50 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// DB_USER=carCarriageAdmin
+// DB_PASS=wzkn745EiP1uqawy
+
+// MongoDb
+
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.tyocyp7.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try{
+        const carCollection = client.db('carsDatabase').collection('cars');
+        const categoryCollection = client.db('carsDatabase').collection('categories');
+
+        app.get('/cars', async(req, res) => {
+            const filter = {};
+            const result = await carCollection.find(filter).toArray();
+            res.send(result);
+        })
+
+        app.get('/categories', async(req, res) => {
+            const filter = {};
+            const result = await categoryCollection.find(filter).toArray();
+            res.send(result);
+        })
+
+
+
+        app.get('/cars/:id', async(req, res) => {
+            const id = req.params.id;
+            const filter = { _id: ObjectId(id) };
+            const result = await carCollection.findOne(filter);
+            res.send(result);
+        })
+    }
+
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+
 app.get('/', (req, res) => {
     res.send('Server is running');
 })
