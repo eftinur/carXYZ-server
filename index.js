c820bcbb5b6c270feddc9514a5e03aa23a347821
eftinur <eftinur90@gmail.comm>
2022-11-29 11:05:33 +0600

    added JWT to verify users

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -19,11 +19,29 @@ const client = new MongoClient(uri, {
   serverApi: ServerApiVersion.v1,
 });
 
+function verfifyToken(req, res, next) {
+  const authHeader = req.headers.authorization;
+  if(!authHeader) {
+    return res.status(401).send('Unauthorized access');
+  }
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, process.env.ACCESS_TOKEN, function (err, decoded) {
+    if (err) {
+      return res.status(403).send({ message: "Forbidden Access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+}
+
 async function run() {
   try {
     // Database collections
     const carCollection = client.db("carsDatabase").collection("cars");
-    const categoryCollection = client.db("carsDatabase").collection("categories");
+    const categoryCollection = client
+      .db("carsDatabase")
+      .collection("categories");
     const userCollection = client.db("carsDatabase").collection("users");
     const orderCollection = client.db("carsDatabase").collection("orders");
 
@@ -44,44 +62,49 @@ async function run() {
 
     // cars API
     app.get("/cars", async (req, res) => {
-        const category = req.query.category;
-        const query = { category: category };
-        const cars = await carCollection.find(query).toArray();
-        res.send(cars);
-      });
+      const category = req.query.category;
+      const query = { category: category };
+      const cars = await carCollection.find(query).toArray();
+      res.send(cars);
+    });
 
     // users API
-    // token API
-    app.get('/token', async(req, res) => {
-      const email = req.query.email;
-      const query = { email: email};
-      const user = await userCollection.findOne(query);
-      if(user) {
-        const token = jwt.sign({email}, process.env.ACCESS_TOKEN, {expiresIn: '2h'});
-        res.send({accessToken: token});
-      }
-      res.status(403).send({accessToken: ''});
-    })
-
-    app.post('/users', async(req, res) => {
+    app.post("/users", async (req, res) => {
       const user = req.body;
       const result = await userCollection.insertOne(user);
       res.send(result);
-    })
+    });
 
     app.get('/users', async(req, res) => {
-      const filter = {};
-      const result = await userCollection.find(filter).toArray();
+      const type = req.query.type;
+      const query = { accountType: type };
+      const result = await userCollection.find(query).toArray();
       res.send(result);
     })
 
-    // orders API
-    app.post('/orders', async(req, res) => {
-      const order = req.body;
-      const result = await orderCollection.insertOne(order);
-      res.send(result); 
+    app.delete('/users', async(req, res) => {
+      const id = req.params.id;
+      const filter = { _id: ObjectId(id)};
+      const result = await userCollection.deleteOne(filter);
+      res.send(result);
     })
 
+
+    // token API
+    app.get("/token", async (req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+      const user = await userCollection.findOne(query);
+      if (user) {
+        const token = jwt.sign({ email }, process.env.ACCESS_TOKEN, {
+          expiresIn: "1h",
+        });
+        return res.send({ accessToken: token });
+      }
+      res.status(403).send({ accessToken: "" });
+    });
+
+    // orders API
   } 
   
   finally {
