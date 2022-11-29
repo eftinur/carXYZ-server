22aff39deaec166a4df8f7c16a85226993a71219
eftinur <eftinur90@gmail.comm>
2022-11-29 16:03:01 +0600

    added delete API for users collection

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ async function run() {
       res.send(result);
     })
 
-    app.delete('/users', async(req, res) => {
+    app.delete('/users/:id', async(req, res) => {
       const id = req.params.id;
       const filter = { _id: ObjectId(id)};
       const result = await userCollection.deleteOne(filter);
